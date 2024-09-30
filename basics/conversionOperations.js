7c6a8a857c0391c87418b46473cd0a7031d9eb3b
Pawan Tiwari <139790625+pawanti8421@users.noreply.github.com>
2024-09-30 04:50:45 +0000

    second conversin

--- basics/conversionOperations.js
+++ basics/conversionOperations.js
@@ -1,8 +1,8 @@
 let score = "50abc"
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber, valueInNumber);
+// console.log(typeof valueInNumber, valueInNumber);
 
 
 // "33" => 33
@@ -12,7 +12,7 @@ console.log(typeof valueInNumber, valueInNumber);
 
 let isLoggedIn = "Pawan"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false;
 // "" => false;
@@ -20,5 +20,43 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+
+// ******************* Operations *****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Pawan"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
+
+
+
