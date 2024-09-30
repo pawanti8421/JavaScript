43c8d5965225f140f2b1a5bf3b153504e55e6289
Pawan Tiwari <139790625+pawanti8421@users.noreply.github.com>
2024-09-30 04:27:52 +0000

    dataconversion

---
+++ basics/conversionOperations.js
@@ -0,0 +1,24 @@
+let score = "50abc"
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber, valueInNumber);
+
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1: false => 0
+// null => 0
+
+let isLoggedIn = "Pawan"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false;
+// "pawan" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
