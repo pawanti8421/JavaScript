12497375c5f092df11834ecdfcfe951fae6da06f
Pawan Tiwari <139790625+pawanti8421@users.noreply.github.com>
2024-10-02 07:09:27 +0000

    function2

--- basics_3.0/function.js
+++ basics_3.0/function.js
@@ -27,4 +27,36 @@ function loginUserMessage(username = "xyz"){
 }
 
 // console.log(loginUserMessage("Pawan"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+// function calculateCartPrice(...num1){  //rest operator
+//     return num1
+// }
+
+function calculateCartPrice(val1, val2, ...num1){  //rest operator
+    return num1 //=>[500,600]
+}
+//console.log(calculateCartPrice(200, 400, 500, 600));
+
+const user = {
+    username: "Pawan",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject(user)
+// handleObject({
+//     username: "Raja",
+//     price: 399
+// })
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+const myNewArray = [200, 400, 600]
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([2000, 4000, 6000]));
